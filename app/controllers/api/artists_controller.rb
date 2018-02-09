04a3f74f44232f4aa8d321c53e7ff41c88a2206f
babylee2002 <babylee2002@gmail.com>
2018-02-09 13:00:50 -0500

    artist controller methods created

--- app/controllers/api/artists_controller.rb
+++ app/controllers/api/artists_controller.rb
@@ -3,5 +3,31 @@ class Api::ArtistsController < ApplicationController
         @artists = Artist.all 
         render json: @artists
     end 
+
+    def show
+        @artist = Artist.find(params[:id])
+        render json: @artist
+    end
+
+    def create
+        @artist = Artist.create!(artist_params)
+    end
+    def update
+        @artist = Artist.find(params[:id])
+        @artist.update!(artist_params)
+        render json: @artist
+    end
+    def destroy
+        @artist = Artist.find(params[:id]).destroy
+        render status: :ok
+    end
+ 
+    private
+    def artist_params
+        params
+        .require(:artist)
+        .permit(:name, :nationality, :photo_url)
+    end 
+
 end
  
